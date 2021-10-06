a2980090d4dd0940278114da1461ae0e1f287b7a
Woke <66000596+woke999@users.noreply.github.com>
2021-10-06 16:13:02 -0400

    Commit first upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CockCs
+{
+    internal class Program
+    {
+        public static void Message(string text, string label)
+        {
+            try
+            {
+                MessageBox.Show(text, label);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName);
+            }
+        }
+
+        public static void Close()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ fstream.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CockCs
+{
+    internal class fstream
+    {
+        public static bool FolderExists;
+        public static bool FileExists;
+
+        public static void CheckFolderExist(string path)
+        {
+            try
+            {
+                bool exists = Directory.Exists(path);
+                if (!exists)
+                {
+                    //the user could now do a check like
+                    /*
+                     * if (FolderExists)
+                     * {
+                     *   code here
+                     *  }
+                     *  else
+                     *  {
+                     *  
+                     *  }
+                    */
+                    FolderExists = false;
+                }
+                else if (exists)
+                {
+                    FolderExists = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName);
+            }
+        }
+
+        public static void CheckFileExist(string path)
+        {
+            bool exists = File.Exists(path);
+            if (!exists)
+            {
+                FileExists = false;
+            }
+            else if (exists)
+            {
+                FileExists = true;
+            }
+        }
+    }
+}
