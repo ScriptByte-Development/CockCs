e630b3e3a90c1ff85bc2b18bcc2ea7449edecc84
Woke <66000596+woke999@users.noreply.github.com>
2021-10-09 12:09:03 -0400

    Add files via upload

---
+++ Example/CockCsExample/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CockCsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.messageBtn = new System.Windows.Forms.Button();
+            this.compareFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(723, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(642, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // messageBtn
+            // 
+            this.messageBtn.Location = new System.Drawing.Point(1, 35);
+            this.messageBtn.Name = "messageBtn";
+            this.messageBtn.Size = new System.Drawing.Size(75, 23);
+            this.messageBtn.TabIndex = 2;
+            this.messageBtn.Text = "message";
+            this.messageBtn.UseVisualStyleBackColor = true;
+            this.messageBtn.Click += new System.EventHandler(this.messageBtn_Click);
+            // 
+            // compareFile
+            // 
+            this.compareFile.Location = new System.Drawing.Point(82, 35);
+            this.compareFile.Name = "compareFile";
+            this.compareFile.Size = new System.Drawing.Size(92, 38);
+            this.compareFile.TabIndex = 3;
+            this.compareFile.Text = "compare file";
+            this.compareFile.UseVisualStyleBackColor = true;
+            this.compareFile.Click += new System.EventHandler(this.compareFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.compareFile);
+            this.Controls.Add(this.messageBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private System.Windows.Forms.Button messageBtn;
+        private System.Windows.Forms.Button compareFile;
+    }
+}
+
---
+++ Example/CockCsExample/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CockCs;
+
+namespace CockCsExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CockCs.win32.AllocConsole();
+            CockCs.win32.Update(Application.ProductName, "Console started");
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            CockCs.Program.Close();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        public void messageBtn_Click(object sender, EventArgs e)
+        {
+            CockCs.Program.Message("test", Application.ProductName);
+        }
+
+        private void compareFile_Click(object sender, EventArgs e)
+        {
+            if (CockCs.fstream.FileCompare(@"C:\Users\bossw\source\repos\CockCs\bin\x64\Release\CockCs.dll", @"C:\Users\bossw\source\repos\CockCs\bin\x64\Debug\CockCs.dll"))
+            {
+                MessageBox.Show("files are equal", Application.ProductName);
+                win32.Update(Application.ProductName, "Files are equal");
+            }
+            else
+            {
+                MessageBox.Show("files are not equal", Application.ProductName);
+                win32.Update(Application.ProductName, "Files are not equal");
+            }
+        }
+    }
+}
