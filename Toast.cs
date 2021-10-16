3e15602ea4ea15fa6bb4e94acd426b7464063f96
Woke <66000596+woke999@users.noreply.github.com>
2021-10-15 22:49:26 -0400

    Add files via upload

---
+++ Toast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+
+namespace CockCs
+{
+    public static class Toast
+    {
+        public static void Create(string topMessage, string bottomMessage)
+        {
+            new ToastContentBuilder()
+            .AddArgument("action", "viewConversation")
+            .AddArgument("conversationId", 9813)
+            .AddText(topMessage)
+            .AddText(bottomMessage)
+            .Show();
+        }
+    }
+}
